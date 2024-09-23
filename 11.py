e2ec5df25afd6ec7dccdb60333db3cb7f96dfb7e
Nazhif Berlian Nashrullah <nazhifberlian@gmail.com>
2024-09-23 23:14:41 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,15 +1,13 @@
-string = (input())
-def encode_string(S):
-    seen = set()
+string = input().replace(" ", "").replace("\t", "")
+def encode_string(S):   #Fungsi ini digunakan untuk memproses kalimat yang kita ketik tadi.
+    seen = set()        #Buat Nyimpen kalimat yg baru ditemuin, klo udh ada sebelumnya ga disimpen lagi
     result = []
 
-    for str in S:
-        if str != " ":
-            if str not in seen:
-                seen.add(str)
-                result.append(str)
-
-    return "".join(result)
+    for str in S:           #ini kita buat for loop
+        if str != " ":      #buat mastiin kalimat yg disimpen itu berupa string bukan spasi
+            if str not in seen:     #nah kalo kalimatnya ga kesimpen...
+                seen.add(str)       #bakalan di add di sini lewat si seen
+                result.append(str)  #ini juga fungsinya buat nambahin kalimat yg blm kesimpen, bedannya dia bakal nambahin di akhir kalimat/list
+    return "".join(result)          #sesuai namanya join ngab, ini fungsinya buat gabungin string yang ada di result tadi
 
 print(encode_string(string))
-    
