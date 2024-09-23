12fff43fc6439359779ecc6013ffff56f68852d7
Nazhif Berlian Nashrullah <nazhifberlian@gmail.com>
2024-09-23 23:13:24 +0700

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,13 +1,16 @@
-def decrypt_message(characters, indices):
-    message = ""
-    for index in indices:
-        message += characters[index - 1]
+def decrypt_message(characters, indices):   #fungsi ini digunakan untuk membaca pesan yang terenskripsi dengan cara membaca posisi pesan tersebut menggunakan indices
+    message = ""                            #message ini nanti bakal diisi huruf-huruf yang diambil berdasarkan nomor yang ada.
+
+    for index in indices:                   #dalam pengulangan for ini kita ngeliat semua angka dari si indices ini angka
+        if index == -1:
+            message += " "
+        else:                   
+            message += characters[index]    #Setiap message yang tertulis akan ditambah + dengan banyaknya character, lalu nanti index yang berasal dari indices akan dikurangkan satu
     return message
 
-characters = input("Enter the string of characters: ")
-n = int(input("Enter the number of times to decrypt: "))
-indices = list(map(int, input("Enter the indices separated by spaces: ").split()))
+characters = input()          
+n = int(input())
+indices = list(map(int, input().split()))
 
 decrypted_message = decrypt_message(characters, indices)
-
-print("Decrypted message:", decrypted_message)
+print(decrypted_message)
